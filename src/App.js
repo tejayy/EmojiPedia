1635fcdbdc3eca076d759e9b80324218b0673a3a
Tejas Jaybhaye <117623822+tejayy@users.noreply.github.com>
2023-10-07 16:12:29 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Dict from './dictionary';
+import emojipedia from "./emojipedia";
 
 function App() {
+
+  function creteCard(emojipedia) {
+    return (
+      <Dict
+        key={emojipedia.id}
+        emoji={emojipedia.emoji}
+        name={emojipedia.name}
+        meaning={emojipedia.meaning}
+      />
+    );
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>
+          <span>emojipedia</span>
+        </h1>
+        <dl className="dictionary">{emojipedia.map(creteCard)}</dl>
+      </div>
     </div>
   );
 }
